59b669d725567e9f2a4dbf2c08e3d4ec7aca9b6e
Paras Dhajal <parasdhajal1012@gmail.com>
2025-06-12 15:05:58 +0530

    Create pract2.py

---
+++ pract2.py
@@ -0,0 +1,10 @@
+
+time = 12
+if(time>=8 and time<=11):
+    print("Its Maths class")
+elif(time>=12 and time<=14):
+    print("Its Science class")
+elif(time>=15 and time<=17):
+    print("Its English class")
+else:
+    print("You are at home")
