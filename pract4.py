8fce9381d7c98d955a1279f57128e62844ad5422
parasdhajal <parasdhajal1012@gmail.com>
2025-07-07 18:54:17 +0530

    Attendence list problem

---
+++ pract4.py
@@ -0,0 +1,7 @@
+attendencelest = [1,2,3,4,5,6,7,8,9,10]
+l1 = [11,12,13,14,15]
+print("Attendance list: ", attendencelest)
+print("Total students: ", len(attendencelest))
+print("Attendance list: ", attendencelest[8])
+l2 = attendencelest + l1
+print("Attendance list after adding new students: ", l2)
