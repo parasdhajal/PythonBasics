3160f6f373f085e453b987b25233a89e4dbf6f60
parasdhajal <parasdhajal1012@gmail.com>
2025-07-12 20:42:28 +0530

    Simple big 0 - o(1) program

--- Pract6.py
+++ Pract6.py
@@ -1,3 +1,7 @@
 def multiply_numbers(n):
     return n*n
-print(multiply_numbers(5))
+print(multiply_numbers(100))
+
+
+#Simple big 0 - o(1) program to demonstrate a function that multiplies a number by itself
+## This program has a constant time complexity O(1) because it performs a fixed number of operations regardless of the input size.
