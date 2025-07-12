08b1e5576a2359c3af5b2ce6caf042b845e422f1
parasdhajal <parasdhajal1012@gmail.com>
2025-07-12 21:12:15 +0530

    This program is O(n^2) complexity, which is quadratic in nature.

---
+++ pract8.py
@@ -0,0 +1,8 @@
+def print_items(n):
+    for i in range(n):
+        for j in range(n):
+            print(i,j)
+print_items(10)
+# This program is O(n^2) complexity, which is quadratic in nature.
+            
+            
