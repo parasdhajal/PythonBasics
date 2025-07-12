67ddbf399dfa48126afd27e5334fb4ed74a12b50
parasdhajal <parasdhajal1012@gmail.com>
2025-07-12 21:02:38 +0530

    Simple big O - O(n) program to demonstrate a function that prints numbers from 0 to n-1

--- pract7.py
+++ pract7.py
@@ -2,3 +2,13 @@ def print_items(n):
     for i in range(n):
         print(i)
 print_items(5)
+
+# Simple big O - O(n) program to demonstrate a function that prints numbers from 0 to n-1 that is O(n) in complexity.
+
+def print_items(n):
+    for i in range(n):
+        print(i)
+    for j in range(n):
+        print(j)
+print_items(5)
+# This prograam is for o(2n) complexity, which is equivalent to O(n) in big O notation.
