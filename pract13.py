3fc8f8bcc8f2a31725b8bfa4492e5160804ebd35
parasdhajal <parasdhajal1012@gmail.com>
2025-07-13 10:15:36 +0530

    Array insertion

--- pract13.py
+++ pract13.py
@@ -3,5 +3,5 @@ import array
 myarray = array.array('i', [1, 2, 3, 4, 5])
 print(myarray)
 
-myarray.insert(0,10)
+myarray.insert(0,10) #insert 10 at the beginning also we can specify the index no then insert the element at that index
 print(myarray)
