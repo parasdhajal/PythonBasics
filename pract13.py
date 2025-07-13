c2e31a906b18d2b7d2bdae4885673f69cfca79e4
parasdhajal <parasdhajal1012@gmail.com>
2025-07-13 10:18:03 +0530

    Array insertion

--- pract13.py
+++ pract13.py
@@ -5,3 +5,5 @@ print(myarray)
 
 myarray.insert(0,10) #insert 10 at the beginning also we can specify the index no then insert the element at that index
 print(myarray)
+
+#time complexity and space complexity is O(n) and O(1) respectively because we are inserting an element at the beginning of the array, which requires shifting all other elements by one position. This operation takes linear time in terms of the number of elements in the array, while the space complexity remains constant as we are not using any additional data structures.
