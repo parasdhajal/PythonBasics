3cf9c856de14ca4919b6e22b46ea7d0f93a2f5c0
parasdhajal <parasdhajal1012@gmail.com>
2025-07-14 20:42:38 +0530

    List Practice

--- pract19.py
+++ pract19.py
@@ -6,4 +6,29 @@ print(my_list)
 
 del my_list[2]
 print(my_list)
+
 #print(my_list[2:5])
+
+#List Operators
+a =[0,1]
+a = a* 3
+print(a)
+
+#List Functions
+b = [1, 2, 3, 4, 5]
+print(len(b))
+print(sum(b))
+print(max(b))
+print(min(b))
+
+
+
+mylist = list()
+while(true):
+    inp = input("Enter the number: ")
+    value = float(inp)
+    if value == 0: break
+    mylist.append(value)
+average = sum(mylist) / len(mylist)
+
+print('Average:', average)
